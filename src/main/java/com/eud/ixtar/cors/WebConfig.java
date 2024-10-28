50a45463081cb5fa5a05a8bffaac31ee44de814a
EudaX400 <eudax400@gmail.com>
2024-10-28 11:59:45 +0100

    poner pàgina de front en el cors

--- src/main/java/com/eud/ixtar/cors/WebConfig.java
+++ src/main/java/com/eud/ixtar/cors/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@SuppressWarnings("null") @NonNull CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:4200") 
+                .allowedOrigins("http://127.0.0.1:4200", "https://ixtar.netlify.app") 
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") 
                 .allowedHeaders("*") 
                 .allowCredentials(true);
