49fb11dc2be693b2b0699953980528bcc9a5c69a
EudaX400 <eudax400@gmail.com>
2024-10-30 12:34:40 +0100

    añadir permitedAll

--- src/main/java/com/eud/ixtar/jwt/SecurityConfg.java
+++ src/main/java/com/eud/ixtar/jwt/SecurityConfg.java
@@ -2,6 +2,7 @@ package com.eud.ixtar.jwt;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -23,20 +24,21 @@ public class SecurityConfg {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable()) 
-            .cors(Customizer.withDefaults()) 
-            .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                    .requestMatchers("/api/auth/**").permitAll() 
-                    .anyRequest().authenticated()) 
-            .sessionManagement(sessionManagement -> sessionManagement
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)) 
-            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class); 
-    
-        return http.build();
-    }
+@Bean
+public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(csrf -> csrf.disable()) 
+        .cors(Customizer.withDefaults()) 
+        .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                .requestMatchers("/api/auth/**").permitAll() 
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() 
+                .anyRequest().authenticated()) 
+        .sessionManagement(sessionManagement -> sessionManagement
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)) 
+        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+    return http.build();
+}
     
 
     @Bean
