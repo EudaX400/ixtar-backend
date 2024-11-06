06abe0a3d78e0b547c8f392250e8b07c744594f6
EudaX400 <eudax400@gmail.com>
2024-11-06 13:15:33 +0100

    añadid un owner al proyecto

--- src/main/java/com/eud/ixtar/project/ProjectController.java
+++ src/main/java/com/eud/ixtar/project/ProjectController.java
@@ -1,5 +1,9 @@
 package com.eud.ixtar.project;
 
+import java.security.Principal;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,19 +14,34 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.eud.ixtar.users.User;
+import com.eud.ixtar.users.UserRepository;
+
 @RestController
 @RequestMapping("/api/projects")
 public class ProjectController {
 
     private final ProjectService projectService;
+    private final UserRepository userRepository;
 
-    public ProjectController(ProjectService projectService) {
+    public ProjectController(ProjectService projectService, UserRepository userRepository) {
         this.projectService = projectService;
+        this.userRepository = userRepository;
     }
 
     @PostMapping
-    public ResponseEntity<Project> createProject(@RequestBody Project project) {
-        return ResponseEntity.ok(projectService.createProject(project));
+    public ResponseEntity<Project> createProject(@RequestBody Project project, Principal principal) {
+        Optional<User> ownerOpt = userRepository.findByEmail(principal.getName());
+
+        if (ownerOpt.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
+        User owner = ownerOpt.get();
+        project.setOwner(owner);
+
+        Project savedProject = projectService.createProject(project);
+        return ResponseEntity.ok(savedProject);
     }
 
     @GetMapping("/{id}")
